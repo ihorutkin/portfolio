2690b85d690f32fe8fbd694f574710daa3e3c26b
Ihor Utkin <igorutkin2002@gmail.com>
2024-02-25 22:38:55 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/mainPage';
 import LayoutPage from './pages/layouts';
 import Games from './pages/games';
